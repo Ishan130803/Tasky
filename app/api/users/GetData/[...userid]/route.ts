862871a86729d0a5832473e9252b484090350546
HarshS490 <142019268+HarshS490@users.noreply.github.com>
2024-04-17 12:14:37 +0530

    updating routes for user and adding routes for projects

--- app/api/users/GetData/[...userid]/route.ts
+++ app/api/users/GetData/[...userid]/route.ts
@@ -1,27 +1,17 @@
 import clientPromise from "@/lib/mongodb";
-import { UserModel } from "@/models/user";
-import { AreaChart } from "lucide-react";
 import { NextRequest, NextResponse } from "next/server";
-import mongoose from "mongoose";
 import { getServerSession } from "next-auth";
-import { PushOperator } from "mongodb";
-export const dynamic = 'force-dynamic'
 
-type Params = {
-  userid: string;
-};
-async function getUserId() {
-  const session = await getServerSession();
-  const userid = session?.user.id
-  return userid
+export const dynamic = "force-dynamic";
+
+interface routeParams {
+  params: {
+    userid: string;
+  };
 }
 
-
-export async function GET(
-  req: Request,
-  context: { params: { userid: Array<string> } }
-) {
-  const userid = context.params.userid[0];
+export async function GET(req: Request, context: routeParams) {
+  const userid = context.params.userid;
 
   try {
     const client = await clientPromise; // Wait for the database connection
@@ -35,20 +25,27 @@ export async function GET(
     if (!user) {
       return new Response("No Such User", { status: 404 });
     }
-
-    return Response.json({ result: [...user.tasks], count: user.tasks.length });
+    if (!user.projects) {
+      await userCollection.updateOne(
+        { userid: userid },
+        { $set: { projects: [] } }
+      );
+      return Response.json({ result: [], count: 0 });
+    }
+    return Response.json({
+      result: [...user.projects],
+      count: user.projects.length,
+    });
   } catch (error) {
-    return new Response("Fatal Error occured while getting user data", {
+    return new Response("Fatal Error occured while getting user projects", {
       status: 500,
     });
   }
 }
 
-export async function PUT(
-  req: NextRequest,
-  context: { params: { userid: Array<string> } }
-) {
-  const userid = context.params.userid[0];
+export async function PUT(req: Request, context: routeParams) {
+  const userid = context.params.userid;
+
   try {
     const client = await clientPromise; // Wait for the database connection
     const db = client.db("Tasky");
@@ -57,29 +54,37 @@ export async function PUT(
     if (!userid) {
       return new Response("user Id must be present", { status: 404 });
     }
-    const data = await req.json();
-
-    for (const item of data) {
-      const { task_id, ...updatedData } = item; // Extract userid and TaskID from item
-      await userCollection.findOneAndUpdate(
-        { userid, "tasks.task_id": task_id }, // Find user with matching userid and task_id
-        { $set: { "tasks.$": item } } // Update the matching task
+    const user = await userCollection.findOne({ userid });
+    if (!user) {
+      return new Response("No Such User", { status: 404 });
+    }
+    if (!user.projects) {
+      await userCollection.updateOne(
+        { userid: userid },
+        { $set: { projects: [] } }
       );
     }
 
-    return new Response("Updated Data", { status: 200 });
+    const data = await req.json();
+
+    for (const item of data) {
+      const { projectid, ...updatedData } = item; // Extract userid and TaskID from item
+      await userCollection.findOneAndUpdate(
+        { userid, "projects.projectid": projectid }, // Find user with matching userid and task_id
+        { $set: { "projects.$": item } } // Update the matching task
+      );
+    }
+    return new Response("Updated User Projects", { status: 200 });
   } catch (error) {
-    return new Response("Fatal Error occured while getting user data", {
+    return new Response("Fatal Error occured while updating user Projects", {
       status: 500,
     });
   }
 }
 
-export async function POST(
-  req: NextRequest,
-  context: { params: { userid: Array<string> } }
-) {
-  const userid = context.params.userid[0];
+export async function POST(req: Request, context: routeParams) {
+  const userid = context.params.userid;
+
   try {
     const client = await clientPromise; // Wait for the database connection
     const db = client.db("Tasky");
@@ -88,48 +93,37 @@ export async function POST(
     if (!userid) {
       return new Response("user Id must be present", { status: 404 });
     }
-    const data = await req.json();
-    const userPresent = await userCollection.findOne({userid:userid});
-    if(userPresent){
-
+    const user = await userCollection.findOne({ userid });
+    if (!user) {
+      return new Response("No Such User", { status: 404 });
+    }
+    if (!user.projects) {
       await userCollection.updateOne(
         { userid: userid },
-        { $push: { tasks: { $each: data } } as unknown as PushOperator<Document>, } // Add taskData to tasks array
-      ).then(()=>{
-        new Response("Inserted Data Successfully", { status: 200 })
-      })
-      .catch((err)=>{
-        new Response("Couldn't insert the data ",err);
-      });
-    }
-    else{
-      await userCollection.insertOne(
-        {
-          userid:userid,
-          tasks:data,
-        }
-      ).then(()=>{
-        new Response("Inserted Data Successfully", { status: 200 })
-      }).catch((err)=>{
-        new Response("Couldn't insert the data ",err);
-      })
+        { $set: { projects: [] } }
+      );
     }
 
-    return new Response("Inserted Data Successfully", { status: 200 });
+    const data = await req.json();
+
+    await userCollection.updateOne(
+      { userid: userid },
+      { $push: { "projects": { $each: data } } } // Add taskData to tasks array
+    );
+
+    return new Response("Inserted Projects Successfully", { status: 200 });
   } catch (error) {
-    return new Response("Fatal Error occured while getting user data", {
+    return new Response("Fatal Error occured while inserting user projects", {
       status: 500,
     });
     // res.status(500).json({ error: 'Error fetching data from MongoDB' });
   }
 }
 
-export async function DELETE(
-  req: NextRequest,
-  context: { params: { userid: Array<string> } }
-) {
-  const userid = context.params.userid[0];
-  const task_id = context.params.userid[1];
+export async function DELETE(req: Request, context: routeParams) {
+  const userid = context.params.userid;
+  const projectid = req.text()
+
   try {
     const client = await clientPromise; // Wait for the database connection
     const db = client.db("Tasky");
@@ -138,13 +132,25 @@ export async function DELETE(
     if (!userid) {
       return new Response("user Id must be present", { status: 404 });
     }
+    const user = await userCollection.findOne({ userid });
+    if (!user) {
+      return new Response("No Such User", { status: 404 });
+    }
+    if (!user.projects) {
+      await userCollection.updateOne(
+        { userid: userid },
+        { $set: { projects: [] } }
+      );
+    }
+
+
     await userCollection.updateOne(
       { userid: userid },
-      { $pull: { tasks: { task_id: task_id } } } // Add taskData to tasks array
+      { $pull: { "projects": { "projectid": projectid } } } 
     );
-    return new Response("Deleted Data Successfully", { status: 200 });
+    return new Response("Deleted Projects Successfully", { status: 200 });
   } catch (error) {
-    return new Response("Fatal Error occured while getting user data", {
+    return new Response("Fatal Error occured while deleting user projects", {
       status: 500,
     });
     // res.status(500).json({ error: 'Error fetching data from MongoDB' });
